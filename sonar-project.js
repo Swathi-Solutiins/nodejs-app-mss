4a7c003c8fa6be6c7ffce00490deb473aaf7c4e5
swash <84779833+swathiGswathi@users.noreply.github.com>
2021-07-01 11:05:28 +0530

    Update sonar-project.js

--- sonar-project.js
+++ sonar-project.js
@@ -1,11 +1,13 @@
 const sonarqubeScanner = require('sonarqube-scanner');
 
 sonarqubeScanner({
-  serverUrl: 'http://18.220.141.205:9000/',
+  serverUrl: 'http://3.108.228.97:9000/',
        options : {
 	    'sonar.projectDescription': 'This is a Node JS application',
 	    'sonar.projectName': 'Node JS Application - Sample',
 	    'sonar.projectKey':'NodeJsMSS',
+	    'sonar.login': 'admin',
+	    'sonar.password': 'swash',
             'sonar.projectVersion':'1.0',
 	    'sonar.language':'js',
             'sonar.sourceEncoding':'UTF-8',
